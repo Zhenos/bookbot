0ffacaa7bd369300ad180d29df8ce51a226f9880
Zhenos <zhenos1832@gmail.com>
2025-01-26 15:34:17 -0800

    Passed lesson 10

--- main.py
+++ main.py
@@ -3,8 +3,16 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(chars_dict)
-
+    characters = []
+    for char in chars_dict:
+        if char.isalpha():
+            characters.append({"letter": char, "num":chars_dict[char]})
+    characters.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
+    for i in range(len(characters)):
+        print(f"The '{characters[i]['letter']}' character was found {characters[i]['num']} times")
+    print("--- End report ---")
 
 def get_num_words(text):
     words = text.split()
@@ -19,12 +27,12 @@ def get_chars_dict(text):
         else:
             chars[lowered] = 1
     return chars
-    
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
-
+def sort_on(dict):
+    return dict["num"]
 
 main()
